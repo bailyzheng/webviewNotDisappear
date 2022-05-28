a1baa2602961b32174300f56cbb851f83c6fd8a2
stones Zheng <eezhenglei@126.com>
2022-05-28 23:40:52 +0800

    init

---
+++ lib/page_mhtc_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MhtcColorPage extends StatelessWidget {
+  const MhtcColorPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Text("color"),
+    );
+  }
+}
---
+++ lib/page_mhtc_web.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled3/page_mhtc_color.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MhtcWebPage extends StatefulWidget {
+  MhtcWebPage({Key? key}): super(key: key);
+
+  @override
+  MhtcWebPageState createState() => MhtcWebPageState();
+}
+
+class MhtcWebPageState extends State<MhtcWebPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+              return MhtcColorPage();
+            }));
+          },
+          child: Text("Title"),
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: "https://bing.com/",
+          javascriptMode: JavascriptMode.unrestricted,
+        );
+      }),
+    );
+  }
+}
